30a84b5e30837141fb3cf2da3f3051f2d24c1ef4
Silence Dogood <mrsdogood@mail.com>
2015-06-25 23:19:42 -0400

    Implemented second half of CG Optimization

--- src/main/java/com/github/mrsdogood/hessianfree/ConjugateGradientOptimizer.java
+++ src/main/java/com/github/mrsdogood/hessianfree/ConjugateGradientOptimizer.java
@@ -6,12 +6,14 @@ import org.ejml.data.DenseMatrix64F;
 import static org.ejml.ops.CommonOps.add;
 import static org.ejml.ops.CommonOps.multTransA;
 import static org.ejml.ops.CommonOps.changeSign;
+import static org.ejml.ops.CommonOps.insert;
 
 public class ConjugateGradientOptimizer extends Optimizer<ConjugateGradientOptimizable>{
     private ConjugateGradientOptimizable optimizable;
     private int i;
     private DenseMatrix64F b, d;
-    private DenseMatrix64F tmp1x1, tmpNx1;
+    private DenseMatrix64F tmp1x1, tmpNx1, grad;
+    private double alpha;
     public ConjugateGradientOptimizer(ConjugateGradientOptimizable optimizable,
         RowD1Matrix64F initialConditions){
         super(optimizable, initialConditions);
@@ -19,6 +21,7 @@ public class ConjugateGradientOptimizer extends Optimizer<ConjugateGradientOptim
         b = new DenseMatrix64F(optimizable.dim(), 1);
         optimizable.getB(b);
         d = new DenseMatrix64F(optimizable.dim(), 1);
+        grad = new DenseMatrix64F(optimizable.dim(), 1);
         tmp1x1 = new DenseMatrix64F(1, 1);
         tmpNx1 = new DenseMatrix64F(optimizable.dim(), 1);
     }
@@ -32,19 +35,53 @@ public class ConjugateGradientOptimizer extends Optimizer<ConjugateGradientOptim
     }
 
     private void firstStep(RowD1Matrix64F x, RowD1Matrix64F out){
-        getFunction().getATimes(x, d);      // d = A * x
-        add(d,b,d);                         // d = d + b
-        changeSign(d);
-        multTransA(d,d,tmp1x1);             // tmp1x1 = d^T * d
-        double alphaNum = tmp1x1.get(0);
+        calcGrad(x);
+        if(gradIsZero()){
+            insert(x,out,0,0); // out = x
+            return;
+        }
+        changeSign(grad, d);        // d = -grad
+        calcAlphaAndUpdateX(x, out);
+    }
+
+    private void calcGrad(RowD1Matrix64F x){
+        getFunction().getATimes(x, grad); // grad = A * x
+        add(grad,b,grad);                 // grad = A * x + b
+    }
+
+    private boolean gradIsZero(){
+        int dim = grad.getNumRows();
+        for(int i = 0; i < dim; i++){
+            if(grad.get(i)!=0)
+                return false;
+        }
+        return true;
+    }
+
+    // requires an updated d and grad
+    private void calcAlphaAndUpdateX(RowD1Matrix64F x, RowD1Matrix64F out){
+        multTransA(d,grad,tmp1x1);          // tmp1x1 = d^T * grad
+        double alphaNum = -tmp1x1.get(0);
         getFunction().getATimes(d, tmpNx1); // tmpNx1 = A * d
         multTransA(d,tmpNx1,tmp1x1);        // tmp1x1 = d^T * A * d
         // alpha = (d^T * d) / (d^T * A * d)
-        double alpha = alphaNum/tmp1x1.get(0);
-        add(x,alpha,d,out);                // out = x + alpha * d
+        alpha = alphaNum/tmp1x1.get(0);
+        add(x,alpha,d,out); // out = x + alpha * d
     }
 
     private void ithStep(RowD1Matrix64F x, RowD1Matrix64F out){
-        throw new RuntimeException("Not Implemented.");
+        getFunction().getATimes(d, tmpNx1);   // tmpNx1 = A * d
+        calcGrad(x);
+        if(gradIsZero()){
+            insert(x,out,0,0); // out = x
+            return;
+        }
+        multTransA(grad, tmpNx1, tmp1x1); // tmp1x1 = grad^T * A * d
+        double betaNum = tmp1x1.get(0);
+        multTransA(d, tmpNx1, tmp1x1);        // tmp1x1 = d^T * A * d
+        // beta = [grad^T * A * d] / (d^T * A * d)
+        double beta = betaNum/tmp1x1.get(0);
+        add(-1.0, grad, beta, d, d);      // d' = -grad + beta * d
+        calcAlphaAndUpdateX(x, out);
     }
 }
--- src/test/java/com/github/mrsdogood/hessianfree/ConjugateGradientOptimizerTest.java
+++ src/test/java/com/github/mrsdogood/hessianfree/ConjugateGradientOptimizerTest.java
@@ -1,12 +1,18 @@
 package com.github.mrsdogood.hessianfree;
 
 import junit.framework.TestCase;
+import java.util.Random;
 
 import org.ejml.data.RowD1Matrix64F;
 import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.RandomMatrices;
+
+import static org.ejml.ops.CommonOps.multTransA;
 
 public class ConjugateGradientOptimizerTest extends TestCase {
     public static final double MAX_ERROR = 1.0e-3;
+    public static final long RAND_SEED = -2309487539010L;
+
     public void testOptimizer(){
         DenseMatrix64F a = new DenseMatrix64F(new double[][]{{5, 4},{4,5}});
         DenseMatrix64F b = new DenseMatrix64F(new double[][]{{-1},{2}});
@@ -18,5 +24,42 @@ public class ConjugateGradientOptimizerTest extends TestCase {
             new double[]{0.55257, -0.66985});
         assertEquals("f(x1):", -1.48774, 
             f.evaluate(optimizer.getCurrentBest()), MAX_ERROR);
+    }
+
+    public void testOptimizerMore(){
+        DenseMatrix64F a = new DenseMatrix64F(new double[][]{{5, 4},{4,5}});
+        DenseMatrix64F b = new DenseMatrix64F(new double[][]{{-1},{2}});
+        DenseMatrix64F x0 = new DenseMatrix64F(new double[][]{{4},{3}});
+        Quadratic f = new Quadratic(a,b,0);
+        ConjugateGradientOptimizer optimizer = new ConjugateGradientOptimizer(f, x0);
+        optimizer.optimize(1);
+        double curBest = f.evaluate(optimizer.getCurrentBest());
+        for(int i = 2; i < 10; i++){
+            optimizer.optimize(1);
+            double fxi = f.evaluate(optimizer.getCurrentBest());
+            if(fxi > curBest+MAX_ERROR)
+                fail(i+"th iteration failed; larger than previous opt.");
+            curBest = fxi;
+        }
+    }
+
+    public void testOptimizer9x9(){
+        Random r = new Random(RAND_SEED);
+        DenseMatrix64F aPart = RandomMatrices.createGaussian(9, 9, 0, 100, r);
+        DenseMatrix64F a = new DenseMatrix64F(9,9);
+        multTransA(aPart,aPart,a);
+        DenseMatrix64F b = RandomMatrices.createGaussian(9, 1, 0, 100, r);
+        DenseMatrix64F x0 = RandomMatrices.createGaussian(9, 1, 0, 100, r);
+        Quadratic f = new Quadratic(a,b,0);
+        ConjugateGradientOptimizer optimizer = new ConjugateGradientOptimizer(f, x0);
+        optimizer.optimize(1);
+        double curBest = f.evaluate(optimizer.getCurrentBest());
+        for(int i = 2; i < 20; i++){
+            optimizer.optimize(1);
+            double fxi = f.evaluate(optimizer.getCurrentBest());
+            if(fxi > curBest+MAX_ERROR)
+                fail(i+"th iteration failed; larger than previous opt.");
+            curBest = fxi;
+        }
     }
 }
