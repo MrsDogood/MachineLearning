44ef930d6a23007d65b96f4e4c9f795eb7435e35
Silence Dogood <mrsdogood@mail.com>
2015-07-22 21:33:01 -0400

    Added an example.

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 I'm trying to understand HF optimization, so I'm creating a repo to experiment in.
 
 ## Building
-This project depends on Maven and Make and only builds in linux at the moment.
+This project depends on Maven and Make and only builds in Linux at the moment.
 Run the ./build.sh script.
+
+## Example
+You'll find an example for this project in 
+`src/main/java/com/github/mrsdogood/example/Main.java`.
+I know that's a long file path; I hate maven conventions too.
+The example can be compiled and executed with the command:
+
+    mvn compile exec:java
---
+++ src/main/java/com/github/mrsdogood/example/Main.java
@@ -0,0 +1,66 @@
+package com.github.mrsdogood.example;
+
+import com.github.mrsdogood.hessianfree.*;
+import com.github.mrsdogood.neural.*;
+
+import org.ejml.data.*;
+
+import java.util.*;
+
+
+public class Main{
+    // Training data
+    // The neural net will be given this data and should learn to reproduce the
+    // outputs given the inputs.
+    public static final double[][] INPUTS = {
+        {0, 0, 0},
+        {1, 1, 0},
+        {0, 1, 1},
+        {0, 0, 1}
+    };
+    public static final double[][] OUTPUTS = {
+        {0},
+        {1},
+        {1},
+        {0}
+    };
+
+    public static void main(String[] args){
+        // Create a neural net.
+        Random r = new Random();
+        FeedForwardNeuralNet nn = new FeedForwardNeuralNet(r, 3, 3, 1);
+        DenseMatrix64F initialWeights = nn.getWeights();
+
+        // Setup an error function to measure how good/bad the neural net is
+        // based on the training data.
+        FeedForwardNeuralNetErrorFunction errorFunction =
+            new FeedForwardNeuralNetErrorFunction(nn);
+        for(int i = 0; i < INPUTS.length; i++)
+            errorFunction.addTrainingSet(INPUTS[i], OUTPUTS[i]);
+
+        System.out.println("Pre-training error: "+
+            errorFunction.evaluate(nn.getWeights()));
+
+        // Create and use an optimizer to minimize the error function
+        // i.e. optimize the neural net
+        final double learningRate = 5;
+        GradientDescentOptimizer optimizer = new GradientDescentOptimizer(
+            errorFunction, initialWeights, learningRate);
+        final int optimizeTimes = 50;
+        optimizer.optimize(optimizeTimes);
+        nn.setWeights(optimizer.getCurrentBest());
+
+        System.out.println("Post-training error: "+
+            errorFunction.evaluate(nn.getWeights()));
+
+        // Manual Tests
+        Scanner kb = new Scanner(System.in);
+        while(true){
+            double[] in = new double[3];
+            System.out.print("Input: ");
+            for(int i = 0; i < in.length; i++)
+                in[i] = kb.nextDouble();
+            System.out.println("Output: "+nn.evaluate(in)[0]);
+        }
+    }
+}
